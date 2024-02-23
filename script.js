c0cf1944598e0558a1fc5ed55b66035064aaacd7
Janardhan Yadav <deepy125@gmail.com>
2024-02-23 23:21:14 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
+  let table = document.getElementById("sampleTable")
+
+  let btn = document.getElementsByTagName("input")
+
+  
+    let row = table.insertRow(0);
+    row.insertCell(0).innerHTML = "New Cell1";
+    row.insertCell(1).innerHTML = "New Cell2";
   
   
 }
